6c6a3a2c119b57b45a5b36ad65754e2563cfa7c7
Julio Laiber de Freitas <julio-lafre@hotmail.com>
2024-05-22 14:36:16 -0300

    Aula 423/426 - Criando rotas e inserindo Apps no Django

--- project/urls.py
+++ project/urls.py
@@ -15,15 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-from django.urls import path
-
-def my_view(request):
-    print("Possivel realizar outras coisas")
-    return HttpResponse('Ola mundo')
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('home.urls')),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
-    path('blog/', my_view)
 ]
